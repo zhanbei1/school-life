b78b3d1f34b9e9a6f35f4a95beb285547a723146
zhanbei <zhanbei@si-tech.com.cn>
2020-08-27 00:35:06 +0800

    项目初次建立

---
+++ src/main/java/com/school/config/Oauth2Config.java
@@ -0,0 +1,13 @@
+package com.school.config;/**
+ * Created by zhanbei on 2020/8/26.
+ */
+
+/**
+ * @ClassName Oauth2Config
+ * @Description
+ * @Author zhanbei
+ * @Date 2020/8/26 23:41
+ * @Version 1.0
+ **/
+public class Oauth2Config {
+}
---
+++ src/main/java/com/school/mybatis_generator/CodeGenerator.java
@@ -0,0 +1,18 @@
+package com.school.mybatis_generator;/**
+ * Created by zhanbei on 2020/8/27.
+ */
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+
+/**
+ * @ClassName CodeGenerator
+ * @Description mybatis-plus代码逆向生成
+ * @Author zhanbei
+ * @Date 2020/8/27 0:17
+ * @Version 1.0
+ **/
+public class CodeGenerator {
+    public static void main(String[] args) {
+        AutoGenerator generator = new AutoGenerator();
+    }
+}
